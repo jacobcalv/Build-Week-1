9e811f8572acaa8620d17221973ac40fde5452ab
jacobcalv <jacobcalvino@outlook.com>
2019-07-17 19:31:10 -0500

    added local files

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// function myFunction() {
+//     var x = document.createElement("INPUT");
+//     document.body.appendChild(x);
+// }
+const theEmptyButton = document.querySelector('button')
+theEmptyButton.textContent = 'Hi';
