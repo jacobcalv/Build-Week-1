c586e974d6dd9f12242b3f2c240b72fb547ed40a
jacobcalv <jacobcalvino@outlook.com>
2019-07-18 20:55:21 -0500

    did a lot of work really should have done more commits

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+var i = 0;
+var images = [];
+var time = 5000;
 
-const theEmptyButton = document.querySelector('emptyButton')
-theEmptyButton.textContent = 'Hi';
+images[0] = 'img/slide1.jpg';
+images[1] = 'img/slide2.jpg';
+images[2] = 'img/slide3.jpg';
+images[3] = 'img/slide4.jpg';
+images[4] = 'img/slide5.jpg';
+
+function changeImg(){
+    document.slide.src = images[i];
+
+    if(i < images.length - 1){
+        i++;
+    }
+    else {
+        i = 0;
+    }
+    setTimeout("changeImg()", time);
+}
+
+window.onload = changeImg;
+
+function myFunc() {
+    document.getElementById("motto").innerHTML = "My Life";
+  }
