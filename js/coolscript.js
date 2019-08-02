0204c92cc26d6ae646d5942539709de603a02ba7
jacobcalv <jacobcalvino@outlook.com>
2019-08-02 10:13:02 -0500

    added tablet and refined moble view

--- js/coolscript.js
+++ js/coolscript.js
@@ -1,5 +1,11 @@
+
+
 function nameFunc(){
     x=document.getElementById('input1');
     y= x.elements['name'].value;
     document.getElementById('output').innerHTML="Hello " + y;
 }
+
+function secret() {
+    document.getElementById("output").innerHTML = "Goodbye " + y;
+  }
